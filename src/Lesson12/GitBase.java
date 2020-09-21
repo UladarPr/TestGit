ff57258aa2127e904d92474507e47dad0a94bb57
eibio <eibiologos@gmail.com>
2020-09-21 13:06:02 +0300

    String word1, word2 is rename to static

--- src/Lesson12/GitBase.java
+++ src/Lesson12/GitBase.java
@@ -1,8 +1,8 @@
 package Lesson12;
 
 public class GitBase {
-    String word1 = "Hello";
-    String word2 = "World!"
+    static String word1 = "Hello";
+    static String word2 = "World!";
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(word1 + word2);
