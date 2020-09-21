532c142b552cd6f739411dde4b48e86f5da5b8b5
eibio <eibiologos@gmail.com>
2020-09-21 13:11:40 +0300

    Method sayHello is added.

--- src/Lesson12/GitBase.java
+++ src/Lesson12/GitBase.java
@@ -3,9 +3,15 @@ package Lesson12;
 public class GitBase {
     static String word1 = "Hello";
     static String word2 = "World!";
+
+    static void sayHello(String word1, String word2) {
+        System.out.println(word1 + " " + word2);
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(word1 + word2);
+        sayHello("Hello", "World!");
 
     }
 }
