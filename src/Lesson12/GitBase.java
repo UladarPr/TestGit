acb5a6d55d8bc545eef699ceaaf6d168c6f01341
eibio <eibiologos@gmail.com>
2020-09-21 20:59:47 +0300

    Words added

--- src/Lesson12/GitBase.java
+++ src/Lesson12/GitBase.java
@@ -5,12 +5,12 @@ public class GitBase {
     static String word2 = "World!";
 
     static void sayHello(String word1, String word2) {
-        System.out.println(word1 + " " + word2);
+        System.out.println( "Method 3 is running: " + word1 + " " + word2);
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println(word1 + " " + word2);
+        System.out.println("Method 1 is running: " + "Hello World!");
+        System.out.println("Method 2 is running: " + word1 + " " + word2);
         sayHello("Hello", "World!");
 
     }
